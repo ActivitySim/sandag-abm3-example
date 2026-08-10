4f4efaddd56e2cdc88ea295f1aab08e3a2068ee8
Will Alexander <will.alexander@rsginc.com>
2026-08-10 11:03:32 -0400

    Primary commit (needs tour MC adjustment)

---
+++ configs/calibration/tour_mode_choice_calib_helper.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+SURVEY_DATA_FOLDER = "activitysim/examples/example_estimation/data_sf/survey_data"
+
+
+def report_tour_mode_choice(context):
+    model_tours = context["tours"]
+    survey_tours = None
+    survey_tours = pd.read_csv(os.path.join(SURVEY_DATA_FOLDER, "override_tours.csv"))
+
+    model_summary = (
+        model_tours.tour_mode.value_counts(normalize=True).sort_index().fillna(0)
+    )
+    if "tour_weight" in survey_tours.columns:
+        survey_summary = survey_tours.groupby("tour_mode").tour_weight.sum()
+        survey_summary = survey_summary / survey_tours.tour_weight.sum()
+    else:
+        survey_summary = survey_tours.groupby("tour_mode").size()
+
+    summary_df = (
+        pd.DataFrame({"model": model_summary, "survey": survey_summary})
+        .reset_index()
+        .rename(columns={"index": "tour_mode"})
+    )
+
+    # plot comparing model and survey distributions
+    summary_df.plot(x="tour_mode", y=["model", "survey"], kind="bar")
+    plt.title("Tour Mode Choice Distribution: Model vs Survey")
+    plt.xlabel("Tour Mode")
+    plt.ylabel("Proportion of Tours")
+    plt.legend(title="Data Source")
+    plt.savefig(
+        os.path.join(context["component_output_dir"], "tour_mode_choice_comparison.png")
+    )
+    plt.close()
+
+
+def get_tour_group(tour_df: pd.DataFrame, person_df: pd.DataFrame) -> pd.Series:
+    ret_series = pd.Series(index=tour_df.index, dtype=str)
+    ret_series.loc[
+        tour_df.primary_purpose.isin(["othmaint", "othdiscr"])
+    ] = tour_df.loc[
+        tour_df.primary_purpose.isin(["othmaint", "othdiscr"])
+    ].primary_purpose.str[
+        3:
+    ]
+    ret_series.loc[
+        tour_df.primary_purpose.isin(["shopping", "escort"])
+    ] = "maint"
+    ret_series.loc[tour_df.primary_purpose.isin(["work", "atwork"])] = tour_df.loc[
+        tour_df.primary_purpose.isin(["work", "atwork"])
+    ].primary_purpose
+    ret_series.loc[tour_df.primary_purpose.isin(["eatout", "social"])] = "discr"
+    ret_series.loc[tour_df.primary_purpose == "school"] = (
+        tour_df.loc[tour_df.primary_purpose == "school"]
+        .person_id.map(person_df.is_university)
+        .map({True: "univ", False: "school"})
+    )
+
+    assert (
+        not ret_series.isna().any()
+    ), f"Tour group could not be determined for some tours:\n{tour_df.loc[ret_series.isna(),['tour_id','person_id','primary_purpose']]}"
+
+    return ret_series
